b3eb1e3924550ff5c986ec3a9c4ca801e48f7afb
Andrew Power <andrewjosephpower@gmail.com>
2020-10-20 10:44:03 -0300

    selfsign done

---
+++ app.js
@@ -0,0 +1,19 @@
+
+require('dotenv').config()
+const express = require('express')
+const fs = require('fs')
+const https = require('https')
+const bodyParser = require('body-parser')
+
+var app = express()
+
+app.use(bodyParser.json())
+
+app.use(express.static(process.env.PUBLIC_PATH))
+
+const httpsServer = https.createServer({
+    key: fs.readFileSync(process.env.KEY_PATH),
+    cert: fs.readFileSync(process.env.CERT_PATH)
+}, app)
+
+httpsServer.listen(process.env.PORT, () => console.log('listening on port ${process.env.PORT}'))
